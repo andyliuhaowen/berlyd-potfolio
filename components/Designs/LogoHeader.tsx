d4242efc67d28d5942c6148e27ff6bdc9c8cdced
Haowen Liu <liu.haowen.andy@gmail.com>
2021-01-15 19:48:30 -0800

    Add logo link

--- components/Designs/LogoHeader.tsx
+++ components/Designs/LogoHeader.tsx
@@ -4,19 +4,23 @@
  */
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LogoHeader() {
   return (
     <header className="hidden absolute md:flex h-36 items-center mt-4 z-50 w-full">
       <div className="ml-10 xl:ml-16 w-16">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          height={128}
-          width={128}
-          layout="responsive"
-          priority={true}
-        ></Image>
+        <Link href="/#mojo">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            height={128}
+            width={128}
+            layout="responsive"
+            priority={true}
+            className="cursor-pointer"
+          ></Image>
+        </Link>
       </div>
     </header>
   );
